b3ff9a0d979314664d7c4936124063eef8aa61f2
Bingtang4aC <yj2937@columbia.edu>
2026-03-01 17:24:32 -0500

    Update and rename BondDuration_File 16.43.48.py to BondDuration_File.py

---
+++ BondDuration_File.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def getBondDuration(y, face, couponRate, m, ppy=1):
+    pvcfsum = 0
+    cf = face * couponRate / ppy
+    
+    n = int(m * ppy)
+    t = np.array(range(1, n + 1))
+    
+    r = y / ppy
+    pvm = (1 + r) ** (-t)
+
+    cf = np.array([cf] * n)
+    cf[-1] += face
+
+    pvcf = pvm * cf
+    pvcfsum += np.sum(pvcf)
+
+    duration= np.sum(t*pvcf)/ pvcfsum
+
+    return duration
